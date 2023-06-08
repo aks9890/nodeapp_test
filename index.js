699427419f6edc448244b6b8a715328740d637b6
aks9890 <114874444+aks9890@users.noreply.github.com>
2023-06-08 23:31:41 +0530

    Update index.js
    commit 3

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From akshay!!!" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello World akshay!!" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works(akshay)!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
