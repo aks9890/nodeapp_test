fa623f93ac54debd47399e3db6578d2a821193be
aks9890 <114874444+aks9890@users.noreply.github.com>
2023-06-08 23:43:23 +0530

    Update index.js
    webhook

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From akshay!!!" }');
+    res.send('{ "response": "Hello From akshay and sumit!!!" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World akshay!!" }');
+    res.send('{ "response": "Hello World akshay$sumit!!" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works(akshay)!" }');
+    res.send('{ "response": " Great!, It works(akshay@sumit)!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
